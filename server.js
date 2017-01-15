54678df0bd3a5746148dbffb498cc72a7b308433
Richard Huang <richardhuang.huang@gmail.com>
2017-01-15 00:25:47 +0000

    added disconnection handling

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 var app=require('express')();
 var http=require('http').Server(app);
 var io=require('socket.io')(http);
+var online=0;
 app.get('/',function(req,res){
     res.sendFile(__dirname+'/index.html');
 });
 io.on('connection',function(sock){
-    console.log('A user has connected from '+sock.request.connection.remoteAddress);
-})
+    online++;
+    console.log('A user has connected from '+sock.request.connection.remoteAddress+'; users online '+online);
+    sock.on('disconnect',function(){
+        online--;
+        console.log('A user disconnected; users online '+online);
+    });
+});
 http.listen(8080,function(){
     console.log('listening on *:8080');
 });
