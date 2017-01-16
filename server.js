14d00acac73bfe00353c6c53d7310d9bd11adf6b
Richard Huang <richardhuang.huang@gmail.com>
2017-01-16 23:16:22 +0000

    global chat functionality

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ io.on('connection',function(sock){
     online++;
     console.log('A user has connected from '+sock.request.connection.remoteAddress+'; users online '+online);
     sock.on('chat message', function(msg){
+        io.emit('chat message',msg);
         console.log('message: ' + msg);
     });
     sock.on('disconnect',function(){
