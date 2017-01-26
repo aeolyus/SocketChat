53a2c3555383db55a6c6c2fc6e96d7de685372c8
Richard Huang <richardhuang.huang@gmail.com>
2017-01-25 21:07:39 -0800

    added environment variable for port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var app=require('express')();
 var http=require('http').Server(app);
 var io=require('socket.io')(http);
 var online=0;
+var port=process.env.POR || 8080;
 app.get('/',function(req,res){
     res.sendFile(__dirname+'/index.html');
 });
@@ -17,6 +18,6 @@ io.on('connection',function(sock){
         console.log('A user disconnected; users online '+online);
     });
 });
-http.listen(8080,function(){
-    console.log('listening on *:8080');
+http.listen(port,function(){
+    console.log('listening on *:port');
 });
